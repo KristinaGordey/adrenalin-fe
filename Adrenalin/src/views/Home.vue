1bd339a4b3568fccb1471ee7573518a96eb0263d
KristinaGordey <144432505+KristinaGordey@users.noreply.github.com>
2025-06-11 13:14:17 +0300

    add carousel

--- Adrenalin/src/views/Home.vue
+++ Adrenalin/src/views/Home.vue
@@ -3,10 +3,21 @@ import "../assets/main.css";
 import HeaderTop from "@/components/HeaderTop.vue";
 import HeaderMiddle from "@/components/HeaderMiddle.vue";
 import HeaderBottom from "@/components/HeaderBottom.vue";
+import Carousel from "@/components/Carousel.vue";
+
+const ImageList = [
+  "../assets/slider/1.jpg",
+  "../assets/slider/2.jpg",
+  "../assets/slider/3.jpg",
+  "../assets/slider/4.jpg",
+  "../assets/slider/5.jpg",
+];
 </script>
 <template>
   <HeaderTop></HeaderTop>
   <HeaderMiddle></HeaderMiddle>
   <HeaderBottom></HeaderBottom>
+  <Carousel></Carousel>
 </template>
+
 <style scoped></style>
