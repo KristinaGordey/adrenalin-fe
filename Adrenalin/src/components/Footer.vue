219269013330273042bbcd985245e8291f1abdcc
KristinaGordey <144432505+KristinaGordey@users.noreply.github.com>
2025-06-12 10:15:00 +0300

    add footer component

---
+++ Adrenalin/src/components/Footer.vue
@@ -0,0 +1,81 @@
+<template>
+  <footer class="bg-gray-800 text-white py-8" id="footer">
+    <div class="max-w-7xl mx-auto px-4">
+      <div class="flex flex-wrap -mx-4">
+        <!-- Информация -->
+        <div class="w-full md:w-1/4 px-4 mb-6 md:mb-0">
+          <h4 class="text-xl font-semibold mb-4">Информация</h4>
+          <ul class="space-y-2 list-none">
+            <li><a class="hover:underline" href="index.html">Главная</a></li>
+            <li><a class="hover:underline" href="about.html">О нас</a></li>
+            <li>
+              <a class="hover:underline" href="category.html?id=1"
+                >Тренировки</a
+              >
+            </li>
+            <li><a class="hover:underline" href="workers.html">Персонал</a></li>
+            <li><a class="hover:underline" href="cart.html">Расписание</a></li>
+            <li><a class="hover:underline" href="price.html">Прайс</a></li>
+            <li id="openModalBtn" class="cursor-pointer hover:underline">
+              Нужна помощь
+            </li>
+          </ul>
+        </div>
+
+        <!-- Часы работы -->
+        <div class="w-full md:w-1/4 px-4 mb-6 md:mb-0">
+          <h4 class="text-xl font-semibold mb-4">Часы работы</h4>
+          <ul class="space-y-2 list-none">
+            <li>Минск, ул. Иосифа Гошкевича 3</li>
+            <li>Воскресенье 09:00-21:00</li>
+            <li>Понедельник 07:00-23:00</li>
+            <li>Вторник 07:00-23:00</li>
+            <li>Среда 07:00-23:00</li>
+            <li>Четверг 07:00-23:00</li>
+            <li>Пятница 07:00-23:00</li>
+            <li>Суббота 09:00-21:00</li>
+          </ul>
+        </div>
+
+        <!-- Контакты -->
+        <div class="w-full md:w-1/4 px-4 mb-6 md:mb-0">
+          <h4 class="text-xl font-semibold mb-4">Контакты</h4>
+          <ul class="space-y-2 list-none">
+            <li>
+              <a class="hover:underline" href="tel:+375298525312"
+                >+375-29-8525312</a
+              >
+            </li>
+            <li>
+              <a class="hover:underline" href="tel:+375292233854"
+                >+375-29-2233854</a
+              >
+            </li>
+          </ul>
+        </div>
+
+        <!-- Присоединяйтесь к нам -->
+        <div class="w-full md:w-1/4 px-4 mb-6 md:mb-0">
+          <h4 class="text-xl font-semibold mb-4">Присоединяйтесь к нам</h4>
+          <ul class="flex space-x-4">
+            <li>
+              <a href="#" class="text-2xl hover:text-gray-400"
+                ><i class="fa-brands fa-youtube"></i
+              ></a>
+            </li>
+            <li>
+              <a href="#" class="text-2xl hover:text-gray-400"
+                ><i class="fa-brands fa-facebook-f"></i
+              ></a>
+            </li>
+            <li>
+              <a href="#" class="text-2xl hover:text-gray-400"
+                ><i class="fa-brands fa-instagram"></i
+              ></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
--- Adrenalin/src/views/Home.vue
+++ Adrenalin/src/views/Home.vue
@@ -4,12 +4,24 @@ import HeaderTop from "@/components/HeaderTop.vue";
 import HeaderMiddle from "@/components/HeaderMiddle.vue";
 import HeaderBottom from "@/components/HeaderBottom.vue";
 import Carousel from "@/components/Carousel.vue";
+import Footer from "@/components/Footer.vue";
 </script>
 <template>
   <HeaderTop></HeaderTop>
   <HeaderMiddle></HeaderMiddle>
   <HeaderBottom></HeaderBottom>
   <Carousel></Carousel>
+  <section class="workers">
+    <div class="container mx-auto px-4">
+      <div class="mb-5">
+        <h2 class="text-2xl font-bold mb-4">
+          <span>Тренерский состав</span>
+        </h2>
+      </div>
+      <div id="trainersCards" class="flex flex-wrap -mx-4"></div>
+    </div>
+  </section>
+  <Footer></Footer>
 </template>
 
 <style scoped></style>
