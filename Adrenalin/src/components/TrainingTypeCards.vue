e1b9cbd3582f744f75b9ff4f80344715012785d6
AliaksandraVasilevich <aleksandravasilevic9@gmail.com>
2025-06-16 10:27:33 +0300

    fixed buttons of TrainingTypeCards

--- Adrenalin/src/components/TrainingTypeCards.vue
+++ Adrenalin/src/components/TrainingTypeCards.vue
@@ -21,11 +21,12 @@
                     руб.
                 </p>
 
-                <button
+                <a
                     class="text-[clamp(1rem,1.25vw,1.5rem)] border self-end border-gray-300 rounded px-4 py-2 mt-2 hover:bg-gray-100 duration-200"
+                    href=""
                 >
                     Записаться
-                </button>
+                </a>
             </div>
         </div>
     </article>
