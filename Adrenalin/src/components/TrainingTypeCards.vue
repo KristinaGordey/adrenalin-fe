03e9275bf12fd4543e7eb74eef47dd696b561d61
AliaksandraVasilevich <aleksandravasilevic9@gmail.com>
2025-06-16 10:12:38 +0300

    fixed styles of TrainingTypeCards and Price page

--- Adrenalin/src/components/TrainingTypeCards.vue
+++ Adrenalin/src/components/TrainingTypeCards.vue
@@ -1,40 +1,43 @@
 <template>
-  <div class="w-full sm:w-1/2 lg:w-1/4 mb-3">
-    <div class="bg-white rounded shadow p-4">
-      <div class="mb-4">
-        <div class="card">
-          <img src="" alt="" class="w-full h-auto object-cover rounded" />
+    <article class="w-full">
+        <div class="bg-white rounded shadow p-5">
+            <div class="flex flex-col h-full gap-y-2">
+                <h2 class="font-semibold">
+                    <div class="hover:underline">{{ training.name }}</div>
+                </h2>
+                <div class="card">
+                    <img
+                        src=""
+                        alt=""
+                        class="w-full h-auto object-cover rounded"
+                    />
+                </div>
+                <p class="text-[clamp(0.7rem,1vw,1.2rem)] text-gray-600">
+                    {{ training.smallDescription }}
+                </p>
+                <p class="">
+                    Цена:
+                    {{ training.price }}
+                    руб.
+                </p>
+
+                <button
+                    class="text-[clamp(1rem,1.25vw,1.5rem)] border self-end border-gray-300 rounded px-4 py-2 mt-2 hover:bg-gray-100 duration-200"
+                >
+                    Записаться
+                </button>
+            </div>
         </div>
-      </div>
-      <div class="flex flex-col justify-between h-full">
-        <h4 class="text-lg font-semibold mb-2">
-          <div class="hover:underline">{{ training.name }}</div>
-        </h4>
-        <p class="text-gray-600 mb-2">{{ training.smallDescription }}</p>
-        <p class="font-semibold mb-4">
-          Цена:
-          {{ training.price }}
-          руб.
-        </p>
-        <div class="flex justify-end">
-          <button
-            class="border border-gray-300 rounded px-4 py-2 hover:bg-gray-100 transition"
-          >
-            Записаться
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
+    </article>
 </template>
 <script setup>
 import { ref } from "vue";
 import catImage from "@/assets/cat.jpg";
 
 const props = defineProps({
-  training: {
-    type: Object,
-    required: true,
-  },
+    training: {
+        type: Object,
+        required: true,
+    },
 });
 </script>
